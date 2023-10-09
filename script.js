cd16f80b25cb7b9ec696ce88f05b5d13e755a754
Gaspard <95536893+gaspjhnt@users.noreply.github.com>
2023-10-09 17:12:12 +0200

    Contact: Form + Map

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ if (document.cookie.indexOf("politique=true") === -1) {
   modal.show();
 }
 
-confirmButton.addEventListener('click', () => {
-    document.cookie = "politique=true";
-    modal.close();
-});
+if (document === "index.html"){
+    confirmButton.addEventListener('click', () => {
+        document.cookie = "politique=true";
+        modal.close();
+    });
+}
+
+
+let map = L.map('map').setView([48.87, 2.30], 14);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+marker = new L.marker([48.870502, 2.304897], {}).bindPopup("Boutique Addidous des Champs Elysées, 59 Avenue des Champs Elysées, 75008 Paris").addTo(map);
+
+map.on();
